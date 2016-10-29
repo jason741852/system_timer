51ef1aff34059516ccb567cfe5394f3881ecc9ec
ksc19 <ksc19@asb9700u-a04.csil.sfu.ca>
2016-10-28 23:00:21 -0700

    done

--- hr-timer.c
+++ hr-timer.c
@@ -17,7 +17,7 @@ unsigned long long timespecDiff(struct timespec *timeA_p, struct timespec *timeB
 }
 void min_func_call(){}
 
-void process_switch_timer();
+unsigned long long process_switch_timer();
 unsigned long long *result2;
 
 void* thread0Call();
@@ -35,93 +35,108 @@ struct timespec stopT1;
 unsigned long long resultT0;
 unsigned long long resultT1;
 
+
 int main()
 {
-//
-// //--------------------------------Part 1--------------------------------
-// // timespec has 2 members tv_sec and tv_nsec that represent the number of seconds and nanoseconds elapsed, respectively
-// struct timespec start;
-// struct timespec stop;
-// unsigned long long result; //64 bit integer
-// ///////////////////////////////////////////////////////////////////////////////////////////
-//
-// ///////////////////////////////////////////////////////////////////////////////////////////
-// // The first clock is CLOCK_REALTIME,
-// // which records the start and stop time of the system clock after sleeping for 1 second,
-// // This should only be used to disply a time on screen
-// // because it might causes errors when user change system clock in run-time
-// ///////////////////////////////////////////////////////////////////////////////////////////
-// clock_gettime(CLOCK_REALTIME, &start);
-// sleep(1);
-// clock_gettime(CLOCK_REALTIME, &stop);
-//
-// result=timespecDiff(&stop,&start);
-//
-// printf("CLOCK_REALTIME Measured: %llu\n",result);
-//
-// ///////////////////////////////////////////////////////////////////////////////////////////
-// // The second clock is CLOCK_MONOTONIC,
-// // which records the absolute elapsed time of the system,
-// // this is useful when a time-dependent function needs to be carried out,
-// // as user cannot alter this clock
-// ///////////////////////////////////////////////////////////////////////////////////////////
-// clock_gettime(CLOCK_MONOTONIC, &start);
-// sleep(1);
-// clock_gettime(CLOCK_MONOTONIC, &stop);
-//
-// result=timespecDiff(&stop,&start);
-//
-// printf("CLOCK_MONOTONIC Measured: %llu\n",result);
-//
-// ///////////////////////////////////////////////////////////////////////////////////////////
-// // The third clock is CLOCK_PROCESS_CPUTIME_ID,
-// // which records ONLY the elapsed time when the process is being executed in the CPU,
-// // this is useful when executing multiple process and when user want to mearsure the time elapsed
-// // for a particular process
-// ///////////////////////////////////////////////////////////////////////////////////////////
-// clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
-// sleep(1);
-// clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &stop);
-//
-// result=timespecDiff(&stop,&start);
-//
-// printf("CLOCK_PROCESS_CPUTIME_ID Measured: %llu\n",result);
-//
-// ///////////////////////////////////////////////////////////////////////////////////////////
-// // The forth clock is CLOCK_THREAD_CPUTIME_ID,
-// // which is simlar to the last one but instead records the time a thread is being executed in CPU,
-// // this is useful when a program uses multiple threads
-// ///////////////////////////////////////////////////////////////////////////////////////////
-// clock_gettime(CLOCK_THREAD_CPUTIME_ID, &start);
-// sleep(1);
-// clock_gettime(CLOCK_THREAD_CPUTIME_ID, &stop);
-//
-// result=timespecDiff(&stop,&start);
-//
-// printf("CLOCK_THREAD_CPUTIME_ID Measured: %llu\n",result);
-//
-//
-// //--------------------------------Part 2--------------------------------
-// clock_gettime(CLOCK_MONOTONIC, &start);
-// min_func_call();
-// clock_gettime(CLOCK_MONOTONIC, &stop);
-//
-// result=timespecDiff(&stop,&start);
-//
-// printf("Minmal Function Call Measured: %llu\n",result);
-//
-// //--------------------------------Part 3--------------------------------
-// clock_gettime(CLOCK_MONOTONIC, &start);
-// getpid();
-// clock_gettime(CLOCK_MONOTONIC, &stop);
-//
-// result=timespecDiff(&stop,&start);
-//
-// printf("Minmal System Call Measured: %llu\n",result);
+
+//--------------------------------Part 1--------------------------------
+// timespec has 2 members tv_sec and tv_nsec that represent the number of seconds and nanoseconds elapsed, respectively
+struct timespec start;
+struct timespec stop;
+unsigned long long result; //64 bit integer
+///////////////////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// The first clock is CLOCK_REALTIME,
+// which records the start and stop time of the system clock after sleeping for 1 second,
+// This should only be used to disply a time on screen
+// because it might causes errors when user change system clock in run-time
+///////////////////////////////////////////////////////////////////////////////////////////
+clock_gettime(CLOCK_REALTIME, &start);
+sleep(1);
+clock_gettime(CLOCK_REALTIME, &stop);
+
+result=timespecDiff(&stop,&start);
+
+printf("CLOCK_REALTIME Measured: %llu\n",result);
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// The second clock is CLOCK_MONOTONIC,
+// which records the absolute elapsed time of the system,
+// this is useful when a time-dependent function needs to be carried out,
+// as user cannot alter this clock
+///////////////////////////////////////////////////////////////////////////////////////////
+clock_gettime(CLOCK_MONOTONIC, &start);
+sleep(1);
+clock_gettime(CLOCK_MONOTONIC, &stop);
+
+result=timespecDiff(&stop,&start);
+
+printf("CLOCK_MONOTONIC Measured: %llu\n",result);
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// The third clock is CLOCK_PROCESS_CPUTIME_ID,
+// which records ONLY the elapsed time when the process is being executed in the CPU,
+// this is useful when executing multiple process and when user want to mearsure the time elapsed
+// for a particular process
+///////////////////////////////////////////////////////////////////////////////////////////
+clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
+sleep(1);
+clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &stop);
+
+result=timespecDiff(&stop,&start);
+
+printf("CLOCK_PROCESS_CPUTIME_ID Measured: %llu\n",result);
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// The forth clock is CLOCK_THREAD_CPUTIME_ID,
+// which is simlar to the last one but instead records the time a thread is being executed in CPU,
+// this is useful when a program uses multiple threads
+///////////////////////////////////////////////////////////////////////////////////////////
+clock_gettime(CLOCK_THREAD_CPUTIME_ID, &start);
+sleep(1);
+clock_gettime(CLOCK_THREAD_CPUTIME_ID, &stop);
+
+result=timespecDiff(&stop,&start);
+
+printf("CLOCK_THREAD_CPUTIME_ID Measured: %llu\n",result);
+
+
+//--------------------------------Part 2--------------------------------
+int n=0;
+FILE* fminfnc = fopen("minFuncRecord.txt", "w");
+for(n;n<500;n++){
+  clock_gettime(CLOCK_MONOTONIC, &start);
+  min_func_call();
+  clock_gettime(CLOCK_MONOTONIC, &stop);
+
+  result=timespecDiff(&stop,&start);
+  fprintf(fminfnc,"%llu\n",result);
+}
+
+
+printf("Minmal Function Call Measured: %llu\n",result);
+
+//--------------------------------Part 3--------------------------------
+FILE* fsyscall = fopen("fsyscallRecord.txt", "w");
+for(n=0;n<500;n++){
+  clock_gettime(CLOCK_MONOTONIC, &start);
+  getpid();
+  clock_gettime(CLOCK_MONOTONIC, &stop);
+
+  result=timespecDiff(&stop,&start);
+  fprintf(fsyscall,"%llu\n",result);
+}
+printf("Minmal System Call Measured: %llu\n",result);
 
 //--------------------------------Part 4--------------------------------
-process_switch_timer();
-
+// int j=0;
+// FILE* fp = fopen("process_switch_timerP.txt", "w");
+// unsigned long long resultP;
+// for(j;j<500;j++){
+//   resultP = process_switch_timer();
+//   fprintf(fp,"%llu\n",resultP);
+// }
 
 
 //--------------------------------Part 5--------------------------------
@@ -152,9 +167,9 @@ return 0;
 
 
 void* thread0Call(){
-
+  FILE* thredRecord = fopen("thredRecord.txt", "w");
   int i=0;
-  while(i<10){
+  while(i<500){
     pthread_mutex_lock(&lock);
     while(state!=0){
       pthread_cond_wait(&cond0, &lock);
@@ -163,10 +178,9 @@ void* thread0Call(){
     pthread_mutex_unlock(&lock);
     clock_gettime(CLOCK_MONOTONIC, &stopT1);
     resultT1=timespecDiff(&stopT1,&startT1);
-    printf("resultT1= %llu\n",resultT1);
+    fprintf(thredRecord,"%llu\n",resultT1);
 
     num = 0;
-    //    printf("%d\n",num);
     pthread_mutex_lock(&lock);
     state = 1;
     pthread_cond_signal(&cond1);
@@ -179,21 +193,17 @@ void* thread0Call(){
 
 void* thread1Call(){
   int j=0;
-  while(j<10){
+  while(j<500){
     pthread_mutex_lock(&lock);
     while(state!=1){
       pthread_cond_wait(&cond1, &lock);
     }
     pthread_mutex_unlock(&lock);
-    //resultT1=timespecDiff(&stopT1,&startT1);
-    //printf("resultT1= %llu\n",resultT1);
-
     num = 1;
-    //printf("%d\n",num);
+
     pthread_mutex_lock(&lock);
     state = 0;
     pthread_cond_signal(&cond0);
-    //clock_gettime(CLOCK_MONOTONIC, &startT1);
     pthread_mutex_unlock(&lock);
     clock_gettime(CLOCK_MONOTONIC, &startT1);
     j++;
@@ -201,10 +211,7 @@ void* thread1Call(){
   return NULL;
 }
 
-void process_switch_timer(){
-  FILE* fpp = fopen("process_switch_timerP.txt", "w");
-  FILE* fpc = fopen("process_switch_timerC.txt", "w");
-  FILE* fpavg = fopen("process_switch_timerAvg.txt", "w");
+unsigned long long process_switch_timer(){
   int i = 0;
   int j =0;
   int times = 5;
@@ -248,7 +255,7 @@ void process_switch_timer(){
  if(child_pid==0){
    close(ParenttoChild[0]); // Close output of parent to child
    close(ChildtoParent[1]); // Close input side of parent
-   for(j=0;j<100;j++){
+   for(j=0;j<500;j++){
      read(ChildtoParent[0],readChar,sizeof(readChar));
      write(ParenttoChild[1],readChar,(strlen(readChar)));// go to parent here
    }
@@ -264,17 +271,17 @@ void process_switch_timer(){
 
    //timer start
    clock_gettime(CLOCK_MONOTONIC, &start1);
-   for(i=0;i<100;i++){
+   for(i=0;i<500;i++){
      write(ChildtoParent[1],readChar,(strlen(readChar)));// go to child here
      read(ParenttoChild[0],readChar,sizeof(readChar));
    }
 
    read(WriteStopTime[0],&stop1,sizeof(stop1));
+
    wait(NULL);
 
    result1 = timespecDiff(&stop1,&start1);
-   result1=result1/200;
-   printf("%llu\n",result1);
+   result1=result1/1000;
  }
- return;
+ return result1;
 }
